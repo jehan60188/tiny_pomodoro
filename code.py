4d21162c4ddfe91ce5ba67da6fc554494f7548e9
jehan60188 <21280121+jehan60188@users.noreply.github.com>
2024-09-04 10:59:06 -0500

    added L/R buttons
    Added logic to read L/R button and change what is displayed, but not the actual state stack behavior

--- code.py
+++ code.py
@@ -7,13 +7,7 @@ import _music
 import time
 
 
-# Compatibility with both CircuitPython 8.x.x and 9.x.x.
-# Remove after 8.x.x is no longer a supported release.
-try:
-    from i2cdisplaybus import I2CDisplayBus
-except ImportError:
-    print('asdf')
-    from displayio import I2CDisplay as I2CDisplayBus
+from i2cdisplaybus import I2CDisplayBus
     
 import terminalio
 from adafruit_display_text import label
@@ -41,22 +35,9 @@ def setup():
         switch.direction = Direction.INPUT
         switch.pull = Pull.UP
         switches.append(switch)
-        
     return piezo, W, H, display, splash, switches
 
-def make_border(splash):
-    color_bitmap = displayio.Bitmap(W, H, 1)
-    color_palette = displayio.Palette(1)
-    color_palette[0] = 0xFFFFFF  # White
 
-    bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
-    splash.append(bg_sprite)
-    # Draw a smaller inner rectangle
-    inner_bitmap = displayio.Bitmap(W-2, H-2, 1)
-    inner_palette = displayio.Palette(1)
-    inner_palette[0] = 0x777777  # Black
-    inner_sprite = displayio.TileGrid(inner_bitmap, pixel_shader=inner_palette, x=1, y=1)
-    splash.append(inner_sprite)
 
 
 def make_text(splash, _txt, offset = (1,1)):
@@ -74,8 +55,8 @@ def beep(piezo, switches):
     
     _break = False
     while True:
-        for f in :
-            _break = any([x.value for x in ])
+        for f in eff:
+            _break = any([x.value for x in switches])
             if _break:
                 piezo.duty_cycle = 0
                 return
@@ -119,14 +100,144 @@ def pomodoro(onoff = (20,5), onoff_sec = None, warning_time = 30):
     #play start
     pass
 
+def make_border(splash):
+    color_bitmap = displayio.Bitmap(W, H, 1)
+    color_palette = displayio.Palette(1)
+    color_palette[0] = 0xFFFFFF  # White
 
+    bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
+    splash.append(bg_sprite)
+    # Draw a smaller inner rectangle
+    inner_bitmap = displayio.Bitmap(W-2, H-2, 1)
+    inner_palette = displayio.Palette(1)
+    inner_palette[0] = 0x777777  # Black
+    inner_sprite = displayio.TileGrid(inner_bitmap, pixel_shader=inner_palette, x=1, y=1)
+    splash.append(inner_sprite)
+
+def make_bitmap(splash, fname):
+    make_border(splash)
+    color_bitmap = displayio.Bitmap(W, H, 1)
+    color_palette = displayio.Palette(2)
+    color_palette[0] = 0x000000  # Black
+    color_palette[1] = 0xFFFFFF  # White
+
+    bitmap = displayio.Bitmap(W, H, 1)
+    
+    row = 0
+    with open(fname, 'r') as f:
+        temp = f.readline()
+        while temp:
+            for idx, item in enumerate(temp.split(',')):
+                bitmap[idx, row] = int(item)
+            temp = f.readline()
+            row+=1
+    bg_sprite = displayio.TileGrid(bitmap, pixel_shader=color_palette, x=0, y=0)
+    splash.append(bg_sprite)
+
+
+class Button():
+    def __init__(self, switch):
+        self.switch = switch
+        self.time_down = 0
+        self.state = 'off' #'tap', 'double (tap)', 'long'
+        self.time_cool = 0
+        self.pushes = []
+    
+    def __bool__(self):
+        return self.switch.value
+    
+    def __iadd__(self, delta):
+        self.time_down = self.time_down + delta
+        pass
+    
+    def reset(self):
+        self.state = "off"
+        self.pushes = []
+            
+    def run(self):
+        isdown = self.switch.value
+        curr_time = time.monotonic()
+        if len(self.pushes) == 0:
+            self.pushes.append([isdown, curr_time])
+            return
+        
+        prev_event, prev_time = self.pushes[-1]
+        delta = curr_time - prev_time
+        if delta> 5:
+            self.state = "off"
+            self.pushes = []
+            return
+        if isdown and not prev_event:
+            self.pushes.append([isdown, curr_time])
+        elif not isdown and prev_event:
+            self.pushes.append([isdown, curr_time])
+            
+        if len(self.pushes) < 2:
+            pass #return
+        if isdown and prev_event and delta>1:
+            self.state = "long"
+            self.pushes = []
+            return
+        if not isdown and not prev_event and delta > .5:
+            if len(self.pushes) > 2:
+                if self.pushes[-2][0]:
+                    self.state = "tap"
+                    self.pushes = []
+            return
+        if isdown and not prev_event:# and delta < .2:
+            if len(self.pushes) > 2:
+                if self.pushes[-3][0]:
+                    self.state = "double tap"
+                    self.pushes = []
+            return
+        return
+        
+
+def set_screen(curr_screen):
+    screens = ['lib/my_tunes/tomato.txt', 'lib/my_tunes/dice.txt', 'lib/app/black.txt']
+    make_bitmap(splash, screens[curr_screen])
+     
 piezo, W, H, display, splash, switches = setup()
+make_bitmap(splash, 'lib/my_tunes/tomato.txt')
+
 #_music.play_from_file(piezo, './lib/my_tunes/ffvii_raw.txt', 99*2, switches)
-pomodoro(onoff_sec = (10,4), warning_time = 5)
+#pomodoro(onoff_sec = (10,4), warning_time = 5)
 
 #_music.play_tune(piezo, _music._ffvii, _tempo= 99*2, interrupt_buttons = switches)
 
+prev_time = time.monotonic()
+
+S = Button(switches[0])
+L = Button(switches[1])
+R = Button(switches[1])
+curr_screen = 0
+screens = [] #main menu, pomodoro, d20
+N = 3#len(screens)
 while True:
-    print([x.value for x in switches])
-    time.sleep(0.1) 
-    pass
+    curr_time = time.monotonic()
+    delta = curr_time-prev_time
+    S.run()
+    L.run()
+    if S.state != 'off':
+        B1_state = S.state
+        if B1_state == 'long':
+            curr_screen = 0
+            set_screen(curr_screen)
+        elif B1_state == 'tap':
+            pass
+        elif B1_state == 'double tap':
+            pass
+        S.reset()
+        continue
+    
+    elif L.state != 'off':
+        L.reset()
+        curr_screen +=1
+        curr_screen = curr_screen%N
+        set_screen(curr_screen)
+
+    
+    
+    prev_time = time.monotonic()
+    time.sleep(0.07)
+    
